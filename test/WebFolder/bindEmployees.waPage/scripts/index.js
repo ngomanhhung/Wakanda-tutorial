575397a558790aee1134d485aa889fdcda183075
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-10-14 15:04:30 +0800

    wankanda example
    First commit to githut

---
+++ test/WebFolder/bindEmployees.waPage/scripts/index.js
@@ -0,0 +1,20 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var Bind = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	Bind.click = function Bind_click (event)// @startlock
+	{// @endlock
+		sources.employee.employer.set(sources.company);
+		
+		sources.employee.save();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("Bind", "click", Bind.click, "WAF");
+// @endregion
+};// @endlock
